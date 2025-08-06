cf22226b67621ad77b89f1c09ea0beea408c9fd4
sadio-kanoute <sadio.kanoute@laplateforme.io>
2025-08-06 17:01:18 +0200

    actualisation css

---
+++ send_mail.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nom = htmlspecialchars($_POST['nom']);
+    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+    $message = htmlspecialchars($_POST['message']);
+
+    if ($email) {
+        $to = "sadio.kanoute@laplateforme.io"; // Mets ici ton email
+        $subject = "Message de $nom via Portfolio";
+        $headers = "From: $email\r\nReply-To: $email\r\n";
+        $headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+        $content = "Nom : $nom\nEmail : $email\n\nMessage :\n$message";
+
+        if (mail($to, $subject, $content, $headers)) {
+            echo "Message envoyé avec succès.";
+        } else {
+            echo "Erreur d’envoi.";
+        }
+    } else {
+        echo "Email invalide.";
+    }
+}
+?>
